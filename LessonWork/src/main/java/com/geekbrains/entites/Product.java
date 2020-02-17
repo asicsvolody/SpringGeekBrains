a11c1bdd23c4fb5f2d2d8d793e6fe632a7dcde56
Vladimir Iakimov <yakimovvn@bk.ru>
2020-02-17 12:24:58 +0300

    Add Product entity

---
+++ LessonWork/src/main/java/com/geekbrains/entites/Product.java
@@ -0,0 +1,37 @@
+package com.geekbrains.entites;
+
+/**
+ * Created by IntelliJ Idea.
+ * User: Якимов В.Н.
+ * E-mail: yakimovvn@bk.ru
+ */
+
+public class Product {
+    private Long id;
+    private String title;
+    private Integer cost;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
